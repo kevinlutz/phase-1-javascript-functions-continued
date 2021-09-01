45c92cf1882e35b60582e9b3ce7ed61be4316a25
+ <+>
2021-09-01 16:29:09 -0400

    commit tree 1276c80de8a7106d3f56c3dab247f4838d2a30e9

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(fun = "roller-skate") {
+  return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(mon = "go to the office") {
+  return `This Monday, I will ${mon}.`;
+}
+
+function wrapAdjective(para = "*") {
+  return function inner(arg = "special") {
+    return `You are ${para}${arg}${para}!`;
+  };
+}
+
+const shout = wrapAdjective("||");
